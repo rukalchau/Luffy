5b9727351eceb2b25435d3d6aeed9d57d018c64b
rukalchau <rukalchau@gmail.com>
2024-01-25 04:50:47 +0100

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,39 @@
-(function(document) {
-  var toggle = document.querySelector('.sidebar-toggle');
-  var sidebar = document.querySelector('#sidebar');
-  var checkbox = document.querySelector('#sidebar-checkbox');
+{
+  const toggle = document.querySelector(".sidebar-toggle");
+  const sidebar = document.querySelector("#sidebar");
+  const checkbox = document.querySelector("#sidebar-checkbox");
+  const accordions = document.querySelectorAll(".accordion");
+  // const subjectAccordion = document.querySelectorAll('.subject-accordion');
+  // const shownSubjectAccordion = document.querySelectorAll('.shown-subject-accordion');
 
-  document.addEventListener('click', function(e) {
-    var target = e.target;
+  document.addEventListener(
+    "click",
+    function (e) {
+      const target = e.target;
 
-    if(!checkbox.checked ||
-       sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
+      // if (sidebar.classList.contains("accordion")) console.log("heere");
+      if (
+        !checkbox.checked ||
+        sidebar.contains(target) ||
+        target === checkbox ||
+        target === toggle
+      ) {
+        // console.log(target.classList.contains(".sub-accordion"))
+        return;
+      }
 
-    checkbox.checked = false;
-  }, false);
-})(document);
+      checkbox.checked = false;
+    },
+    false
+  );
+
+  accordions.forEach((accordion) => {
+    const notesAccordion = accordion.querySelector(".sub-accordion");
+    const subAccordion = accordion.querySelector(".sub-sub-accordion");
+    subAccordion.classList.add("w3-hide");
+
+    notesAccordion.addEventListener("click", () => {
+      subAccordion.classList.toggle("w3-hide");
+    });
+  });
+}
